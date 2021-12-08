c86b86af8f73a581777ead4a081fe9eeba7daee2
PabloVallecillos <vallecillospablo@gmail.com>
2021-12-08 23:09:53 +0100

    feat: Init commit.

---
+++ src/components/AppBar.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-app-bar
+      app
+      color="black"
+      dark
+  >
+    <div class="d-flex align-center">
+      <h3>dev</h3>
+    </div>
+
+    <v-spacer></v-spacer>
+
+    <v-btn
+      text
+      to="/news"
+    >
+      <span>News</span>
+    </v-btn>
+    <h1>|</h1>
+    <v-btn
+      text
+      to="/docs"
+    >
+      <span>docs</span>
+    </v-btn>
+    <h1>|</h1>
+    <v-btn
+      text
+      to="/"
+    >
+      <span>home</span>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'app-bar',
+});
+</script>
+
--- src/router/index.ts
+++ src/router/index.ts
@@ -11,13 +11,18 @@ const routes: Array<RouteConfig> = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/news',
+    name: 'News',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    component: () => import(/* webpackChunkName: "news" */ '../views/News.vue')
+  },
+  {
+    path: '/docs',
+    name: 'Docs',
+    component: () => import(/* webpackChunkName: "docs" */ '../views/Docs.vue')
+  },
 ]
 
 const router = new VueRouter({
